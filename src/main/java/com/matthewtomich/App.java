7ff3475d74084dfb2e00c44ad9aa855e073bd359
matthew.tomich <matthew.tomich@gmail.com>
2013-09-28 16:19:00 +0100

    init

---
+++ src/main/java/com/matthewtomich/App.java
@@ -0,0 +1,21 @@
+package com.matthewtomich;
+
+import static spark.Spark.*;
+import spark.*;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+    	get(new Route("/hello") {
+            @Override
+            public Object handle(Request request, Response response) {
+               return "Hello World!";
+            }
+         });
+    }
+}
